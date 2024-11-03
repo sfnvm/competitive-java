bb15f07225fb78cf7b2732b9ab86c943508a1b66
sfnvm <trungpt.dev@gmail.com>
2024-11-03 16:16:33 +0700

    [LeetCode] Q0007_ReverseInteger

---
+++ src/main/java/edu/sfnvm/competitive/leetcode/Q0007_ReverseInteger.java
@@ -0,0 +1,46 @@
+package edu.sfnvm.competitive.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/reverse-integer">
+ * Reverse Integer
+ * </a>
+ */
+public class Q0007_ReverseInteger {
+
+    public static void main(String[] args) {
+        test(1);
+    }
+
+    public static void test(int input) {
+        Solution s = new Solution();
+        var result = s.reverse(input);
+        System.out.println(result);
+    }
+
+    public static class Solution {
+        public int reverse(int x) {
+            boolean neg = false;
+            if (x < 0) {
+                neg = true;
+                x = -x;
+            }
+
+            int prevRevNum = 0;
+            int revNum = 0;
+            while (x != 0) {
+                int curDigit = x % 10;
+                revNum = (revNum * 10) + curDigit;
+
+                // Check overflow
+                if ((revNum - curDigit) / 10 != prevRevNum) {
+                    return 0;
+                }
+
+                prevRevNum = revNum;
+                x = x / 10;
+            }
+
+            return neg ? -revNum : revNum;
+        }
+    }
+}
