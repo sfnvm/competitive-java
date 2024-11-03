faa94a9e37b079d6046d1e43da70c022dc171053
sfnvm <trungpt.dev@gmail.com>
2024-11-03 22:31:23 +0700

    [LeetCode] Q0009_PalindromeNumber

---
+++ src/main/java/edu/sfnvm/competitive/leetcode/Q0009_PalindromeNumber.java
@@ -0,0 +1,79 @@
+package edu.sfnvm.competitive.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/palindrome-number">Palindrome Number</a>
+ */
+public class Q0009_PalindromeNumber {
+
+    public static void main(String[] args) {
+        test(123);
+        test(121);
+        test(1221);
+        test(10);
+        test(1000021);
+        test(1410110141);
+    }
+
+    public static void test(int input) {
+        Solution s = new Solution();
+        var result = s.isPalindrome(input);
+        System.out.println(result);
+    }
+
+    public static class Solution {
+        public boolean naiveIsPalindrome(int x) {
+            if (x < 0) {
+                return false;
+            } else if (x < 10) {
+                return true;
+            }
+
+            StringBuilder strRs = new StringBuilder();
+            while (x / 10 != 0) {
+                strRs.append(x % 10);
+                x = x / 10;
+            }
+            strRs.append(x);
+
+            boolean rs = true;
+            for (int i = 0; i < strRs.length() / 2; i++) {
+                if (strRs.charAt(i) != strRs.charAt(strRs.length() - 1 - i)) {
+                    rs = false;
+                    break;
+                }
+            }
+
+            return rs;
+        }
+
+        public boolean isPalindrome(int x) {
+            if (x < 0) {
+                return false;
+            } else if (x < 10) {
+                return true;
+            }
+
+            int div = 1;
+            int divB = 1;
+            // Detect devider
+            while (x >= (10 * div)) {
+                if (div >= 1000000000) {
+                    break;
+                } else {
+                    div *= 10;
+                }
+            }
+
+            while (true) {
+                if (x == 0 || div == 0) {
+                    return true;
+                }
+                if (x / div != x % 10) {
+                    return false;
+                }
+                x = x % div / 10;
+                div = div / 100;
+            }
+        }
+    }
+}
